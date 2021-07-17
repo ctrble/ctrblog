95a1cec7986df765e5896314e920304e243d87c4
Charlotte Trible <ctrble@gmail.com>
2021-07-16 20:35:57 -0500

    fixing the linter of doom

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  eslint: {
+    // Warning: Dangerously allow production builds to successfully complete even if
+    // your project has ESLint errors.
+    // -- don't care, we're running our own config
+    ignoreDuringBuilds: true,
+  },
+};
