7fbf8127284825f552af7d8369d13b6c58783951
Charlotte Trible <ctrble@gmail.com>
2024-09-30 22:35:16 -0500

    use it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
 import type { Metadata } from "next";
-import { Belanosima, Martian_Mono } from 'next/font/google'
+import { Belanosima, Martian_Mono } from "next/font/google";
 
 import "@/styles/index.scss";
 
 const belanosima = Belanosima({
-  display: 'swap',
-  subsets: ['latin'],
+  display: "swap",
+  subsets: ["latin"],
   variable: "--font-belanosima",
-  weight: ['400', '600', '700'],
-})
+  weight: ["400", "600", "700"],
+});
 
 const martianMono = Martian_Mono({
-  display: 'swap',
-  subsets: ['latin'],
+  display: "swap",
+  subsets: ["latin"],
   variable: "--font-martian-mono",
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800'],
-})
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
 
 export const metadata: Metadata = {
   title: "Developer Party",
