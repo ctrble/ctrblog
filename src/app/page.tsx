73a0f091e533496824a69e6aa9e83df878cb493f
Charlotte Trible <ctrble@gmail.com>
2024-09-30 23:18:46 -0500

    cmon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,7 @@ export default function Home() {
     <main className={styles.main}>
       <div>
         <h1>Hey! I'm Charlotte and I love building websites.</h1>
-        <p>
-          I’ve always been fascinated by technology, especially when it involves
-          solving tricky challenges. My journey into web development began in my
-          teenage years when I wrote pseudo-code in a notebook while on
-          vacation! Since then, I’ve worked as a certified technician, created
-          video games as a hobby, co-founded Flyover Indies (an indie game dev
-          community in Kansas City), and led the development of high-traffic,
-          engaging websites, including a Webby award winner. I find joy in
-          tackling interesting problems, and I’m passionate about clean,
-          consistent code. I love mentoring and learning from fellow engineers,
-          balancing business priorities with technical needs, and creating
-          amazing user experiences. I thrive in collaborative environments and
-          am always eager to dive into new challenges. My favorite tech stack
-          includes React (especially Next.js), SCSS Modules, and various code
-          quality tools, but I’m excited to learn new things and adapt quickly!
-        </p>
+        <p>Making things move on the computer screen still sparks joy.</p>
         <h2>Find Me</h2>
         <ol>
           <li>
