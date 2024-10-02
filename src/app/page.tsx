f6cb142a64a903767a695b650416059742b72849
Charlotte Trible <ctrble@gmail.com>
2024-10-01 21:45:32 -0500

    layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,25 +4,28 @@ import styles from './page.module.scss';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div>
-        <h1>Hey! I'm Charlotte and I love building websites.</h1>
-        <p>Making things move on the computer screen still sparks joy.</p>
-        <h2>Find Me</h2>
-        <ol>
-          <li>
-            <WebLink href='https://github.com/ctrble'>GitHub</WebLink>
-          </li>
-          <li>
-            <WebLink href='https://codepen.io/ctrble'>Codepen</WebLink>
-          </li>
-          <li>
-            <WebLink href='www.linkedin.com/in/charlotte-trible'>
-              LinkedIn
-            </WebLink>
-          </li>
-        </ol>
-      </div>
+    <main className={styles.outer}>
+      <section className={styles.inner}>
+        <div>
+          <h1>Hey! I'm Charlotte and I love building websites.</h1>
+          <p>Making things move on the computer screen still sparks joy.</p>
+          <hr />
+          <h2>Find Me</h2>
+          <ol>
+            <li>
+              <WebLink href='https://github.com/ctrble'>GitHub</WebLink>
+            </li>
+            <li>
+              <WebLink href='https://codepen.io/ctrble'>Codepen</WebLink>
+            </li>
+            <li>
+              <WebLink href='www.linkedin.com/in/charlotte-trible'>
+                LinkedIn
+              </WebLink>
+            </li>
+          </ol>
+        </div>
+      </section>
     </main>
   );
 }
